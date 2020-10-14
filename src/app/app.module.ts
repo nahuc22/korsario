df13c93de4929a1f408f31a4a5a550a1ec89b329
Nahuel Martín Castilla <nahuelcastilla22@gmail.com>
2020-10-14 19:22:22 -0300

    korsario 14/10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +23,7 @@ import { ListPersonaComponent } from './models/list-persona/list-persona.compone
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/nueva-editar-persona/nueva-editar-persona.component.ts
+++ src/app/models/nueva-editar-persona/nueva-editar-persona.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormBuilder , Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-nueva-editar-persona',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevaEditarPersonaComponent implements OnInit {
 
-  constructor() { }
+  persona: FormGroup;
+  idPersona = 0;
+  accion = 'Crear';
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private route: ActivatedRoute) {
+
+    this.persona = this.fb.group({
+        nombre : ['', Validators.required],
+       apellido: ['', Validators.required],
+            dni: ['', Validators.required]
+    });
+
+    if (+this.route.snapshot.paramMap.get('id') > 0) {
+      this.idPersona = +this.route.snapshot.paramMap.get('id');
+    }
   }
 
+  ngOnInit(): void{
+
+    this.esEditar();
+  }
+
+  // tslint:disable-next-line:typedef
+  guardarPersona() {
+  console.log(this.persona);
+  }
+
+  esEditar() {
+    if (this.idPersona > 0) {
+      this.accion = 'Editar';
+      this.persona.patchValue({
+        nombre: 'Nahuel',
+        apellido: 'Castilla',
+        dni: '40238335'
+      });
+    }
+  }
 }
